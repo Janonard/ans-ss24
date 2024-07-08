571cc73495d18beb86978cc56c3e4c268ca04c0a
Jonas <tjonas@mail.uni-paderborn.de>
2024-07-08 17:20:35 +0200

    Changed sending to external class

--- lab5/sml-udp/worker.py
+++ lab5/sml-udp/worker.py
@@ -17,7 +17,8 @@
 from lib.gen import GenInts, GenMultipleOfInRange
 from lib.test import CreateTestData, RunIntTest
 from lib.worker import *
-from scapy.all import Packet, ByteField, IntField, FieldListField, Raw
+from lib.comm import *
+from scapy.all import Packet, ByteField, IntField, FieldListField
 import socket
 
 NUM_ITER   = 8     # TODO: Make sure your program can handle larger values
@@ -31,46 +32,45 @@ class SwitchML(Packet):
     ]
 
 
-def AllReduce(iface, rank, data, result):
+def AllReduce(soc, rank, data, result):
     """
-    Perform in-network all-reduce over ethernet
+    Perform in-network all-reduce over UDP
 
-    :param str  iface: the ethernet interface used for all-reduce
+    :param str    soc: the socket used for all-reduce
     :param int   rank: the worker's rank
     :param [int] data: the input vector for this worker
     :param [int]  res: the output vector
 
     This function is blocking, i.e. only returns with a result or error
     """
-    
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind((f"10.0.0.{rank+1}", 0x4200))
 
     for i in range(0, len(data), CHUNK_SIZE):
         # Send packet
         payload = bytes(SwitchML(rank=rank, data=data[i:i+CHUNK_SIZE]))
-        udp_socket.sendto(payload, ("10.0.1.1", 0x4200))
+        send(soc, payload, ("10.0.1.1", 0x4200))
 
         # Receive answer
-        rec_packet, _ = udp_socket.recvfrom(1024)
+        rec_packet, _ = receive(soc, 1024)
         result[i:i+CHUNK_SIZE] = SwitchML(rec_packet).data
         Log(SwitchML(rec_packet).data)
 
-    udp_socket.close()
-    
-
 def main():
-    iface = 'eth0'
     rank = GetRankOrExit()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((f"10.0.0.{rank+1}", 0x4200))
+
     Log("Started...")
     for i in range(NUM_ITER):
         num_elem = CHUNK_SIZE*3 #GenMultipleOfInRange(2, 2048, 2 * CHUNK_SIZE) # You may want to 'fix' num_elem for debugging
         data_out = GenInts(num_elem)
         data_in = GenInts(num_elem, 0)
-        CreateTestData("eth-iter-%d" % i, rank, data_out)
-        AllReduce(iface, rank, data_out, data_in)
-        RunIntTest("eth-iter-%d" % i, rank, data_in, True) 
+        CreateTestData("udp-iter-%d" % i, rank, data_out)
+        AllReduce(s, rank, data_out, data_in)
+        RunIntTest("udp-iter-%d" % i, rank, data_in, True)
     Log("Done")
+
+    s.close()
 
 if __name__ == '__main__':
     main()
